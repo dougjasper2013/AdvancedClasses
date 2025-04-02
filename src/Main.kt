2111b8391ab61a94bce6ca407892a27003993bf4
Douglas Jasper <doug.jasper@trios.com>
2025-04-02 10:05:31 -0400

    Changed Files

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,38 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+enum class VehicleType { CAR, TRUCK, MOTORCYCLE }
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+interface Drivable {
+    fun drive()
+}
+
+// Custom Exception
+class InvalidOwnerException(message: String) : Exception(message)
+
+class Vehicle<T>(val type: VehicleType, private val owner: T) : Drivable {
+    init {
+        if (owner is String && owner.isBlank()) {
+            throw InvalidOwnerException("Owner name cannot be blank!")
+        }
+    }
+
+    override fun drive() {
+        println("$owner is driving a $type.")
+    }
+}
+
+fun main() {
+    try {
+        val car = Vehicle(VehicleType.CAR, "Alice")
+        val truck = Vehicle(VehicleType.TRUCK, "Bob")
+        val invalidVehicle = Vehicle(VehicleType.MOTORCYCLE, "") // This will throw an exception
+
+        car.drive()
+        truck.drive()
+        invalidVehicle.drive()  // This line won't execute due to the exception above
+    } catch (e: InvalidOwnerException) {
+        println("Error: ${e.message}")
+    } catch (e: Exception) {
+        println("Unexpected error: ${e.message}")
+    } finally {
+        println("Program execution finished.")
     }
 }
